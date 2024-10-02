5a82ca539d7697c2f594c3aa226bae3196fcbc55
hal-wang <hi@hal.wang>
2024-10-02 11:24:57 +0800

    chore: update deps

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,10 +4,7 @@
     <button class="bg-blue-300 px-2 py-1 round hover:bg-blue-500 rounded-md" @click="() => count++">
       ClickMe
     </button>
-    <button
-      class="bg-blue-300 px-2 py-1 round hover:bg-blue-500 rounded-md"
-      @click="() => navToSecondPage()"
-    >
+    <button class="bg-blue-300 px-2 py-1 round hover:bg-blue-500 rounded-md" @click="() => navToSecondPage()">
       To Second Page
     </button>
 
@@ -17,19 +14,19 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed } from 'vue';
-  import { useRouter } from 'vue-router';
-  import Icon from '/@/components/Icon';
-  import { useAppStore } from '/@/store/modules/app';
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import Icon from '/@/components/Icon';
+import { useAppStore } from '/@/store/modules/app';
 
-  const appStore = useAppStore();
-  const count = computed({
-    get: () => appStore.getCount,
-    set: (val: number) => appStore.setCount(val),
-  });
+const appStore = useAppStore();
+const count = computed({
+  get: () => appStore.getCount,
+  set: (val: number) => appStore.setCount(val),
+});
 
-  const router = useRouter();
-  function navToSecondPage() {
-    router.push('/second');
-  }
+const router = useRouter();
+function navToSecondPage() {
+  router.push('/second');
+}
 </script>
